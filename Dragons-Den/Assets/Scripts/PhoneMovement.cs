74df1b316ff3e995b1520a7b76a4cef141dd1939
Boldea Marius <yukki.github@gmail.com>
2022-11-24 17:16:09 +0100

    Movement for phone done

---
+++ Dragons-Den/Assets/Scripts/PhoneMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneMovement : MonoBehaviour
+{
+    [SerializeField] Camera _camera;
+
+    [SerializeField, Tooltip("Character move speed.")] float speed = 1;
+    [SerializeField, Tooltip("Character acceleration.")] float walkAcceleration = 10;
+    [SerializeField, Tooltip("Character deceleration.")] float walkDeceleration = 10;
+    Vector2 velocity = new Vector2();
+
+    private void FixedUpdate()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            if (Input.mousePosition.x > _camera.transform.position.x + 5 * _camera.scaledPixelWidth / 6 && Input.mousePosition.x < _camera.transform.position.x + _camera.scaledPixelWidth)
+            {
+                velocity.x = Mathf.MoveTowards(velocity.x, speed, walkAcceleration * Time.deltaTime);
+            }
+            else if(Input.mousePosition.x < _camera.transform.position.x + _camera.scaledPixelWidth / 6 && Input.mousePosition.x > _camera.transform.position.x)
+            {
+                velocity.x = Mathf.MoveTowards(velocity.x, -speed, walkAcceleration * Time.deltaTime);
+            } 
+        }
+        else
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, walkDeceleration * Time.deltaTime);
+        }
+
+        transform.Translate(velocity * Time.deltaTime);
+    }
+}
