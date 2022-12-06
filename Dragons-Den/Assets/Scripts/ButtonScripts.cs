10c2a32e4db888a97fe2d16cf969400f573203e9
Boldea Marius <yukki.github@gmail.com>
2022-12-06 11:41:36 +0100

    UI changes

--- Dragons-Den/Assets/Scripts/ButtonScripts.cs
+++ Dragons-Den/Assets/Scripts/ButtonScripts.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class ButtonScripts : MonoBehaviour
 {
+    #region Burger_menu_panel
     [SerializeField] Animator animator;
     bool slide;
+    #endregion
+
+    //Burger menu panel that slides
     public void BringMenu()
     {
         animator.SetBool("SlideBack", slide);
         slide = !slide;
+    }
+
+    public void ExitAplicationm()
+    {
+        Application.Quit();
     }
 }
