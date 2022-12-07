b1b32b5f07eb1969d280bba8e9d325e6199f1a6e
Ruben Smith <ruben.smith@ebuynow.com>
2022-12-07 19:11:13 +0100

    Added scenes and main menu functionality

---
+++ Dragons-Den/Assets/MenuNavigator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigator : MonoBehaviour
+{
+    public void LoadRome()
+    {
+        SceneManager.LoadScene("MainRoad", LoadSceneMode.Single);
+    }
+}
